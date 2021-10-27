adadecc3d49f34f8872606d291fa8167c6b9d60e
jwcho <djdlzl93@gmail.com>
2021-10-27 16:48:34 +0900

    rt2_step4

---
+++ 05_rt.tf
@@ -0,0 +1,11 @@
+resource "aws_route_table" "jwcho_rt" {
+  vpc_id = aws_vpc.jwcho_vpc.id
+
+  route = [{
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.jwcho_igw.id
+  }]
+  tags = {
+    "Name" = "jwcho-rt"
+  }
+}
