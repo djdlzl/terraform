57ee296b1cb6eb7f2fc587646835e2b6d8c4669f
jwcho <djdlzl93@gmail.com>
2021-10-29 15:22:11 +0900

    listener_step14

---
+++ 14_lb_listener.tf
@@ -0,0 +1,10 @@
+resource "aws_lb_listener" "jwcho_front-end" {
+  load_balancer_arn = aws_lb.jwcho_lb.arn
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type  = "forward"
+    target_group_arn = aws_lb_target_group.jwcho_lb_tg.arn
+  }
+}
