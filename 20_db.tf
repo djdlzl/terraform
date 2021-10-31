87d8d4af297fbd037bee8f62d69b6105a23507d7
djdlzl <djdlzl93@gmail.com>
2021-10-31 19:21:21 +0900

    step22

--- 20_db.tf
+++ 20_db.tf
@@ -9,6 +9,7 @@ resource "aws_db_instance" "jwcho_db" {
   password = "It123412341!"
   parameter_group_name = "default.mysql5.7"
   availability_zone = "ap-northeast-2a"
+  vpc_security_group_ids   = [aws_security_group.jwcho_websg.id]
   db_subnet_group_name = aws_db_subnet_group.jwcho_db_sb_group.id
   skip_final_snapshot = true
   tags = {
